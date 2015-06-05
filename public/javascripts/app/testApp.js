347bb33a567b92b5f8ae2e2d4f3362922ce6f678
Sara Baumgarten <Sara@Saras-MacBook-Pro-2.local>
2015-06-04 23:51:26 -0400

    Adding more angular

--- public/javascripts/app/testApp.js
+++ public/javascripts/app/testApp.js
@@ -3,3 +3,11 @@ var app = angular.module('myApp', []);
 app.run(function($rootScope) {
     $rootScope.name = "Avidan Ackerson";
 });
+
+app.controller('TemperatureControl', function($scope) {
+    // get names using AngularJS AJAX API
+    $http.get('/data').success(function(data){
+        $scope.temperatureRecords = data;
+        $rootScope.temperatureRecords = data;
+    });
+});
