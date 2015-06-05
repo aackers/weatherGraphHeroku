88ec4f4c66fa3a23f00544bb4d15b81afd1b3d2c
Sara Baumgarten <Sara@Saras-MacBook-Pro-2.local>
2015-06-05 00:04:53 -0400

    Trying to do an http get

--- public/javascripts/app/testApp.js
+++ public/javascripts/app/testApp.js
@@ -4,10 +4,12 @@ app.run(function($rootScope) {
     $rootScope.name = "Avidan Ackerson";
 });
 
-app.controller('TemperatureControl', function($scope) {
+app.controller('TemperatureControl', ['$scope', function($scope) {
     // get names using AngularJS AJAX API
-    $http.get('/data').success(function(data){
-        $scope.temperatureRecords = data;
-        $rootScope.temperatureRecords = data;
-    });
-});
+    $scope.getData = function() {
+        $http.get('/data').success(function(data) {
+            $scope.temperatureRecords = data;
+            $rootScope.temperatureRecords = data;
+        });
+    }
+}]);
